a0a945ffa33687f6c2f108172fe308239e35162e
Techin chompooborisuth <chompooborisuthtechin@gmail.com>
2026-08-16 20:59:01 +0700

    Add: merge-intervals - submission-8

---
+++ Data Structures & Algorithms/merge-intervals/submission-8.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        let result = []
+        intervals.sort((a, b) => a[0] - b[0])
+        console.log(intervals)
+        let start_interval = intervals[0][0]
+        let end_interval = intervals[0][1]
+
+        for (const interval of intervals.slice(1)){
+            // [1, 3], [1, 5], [6, 7]
+            if (interval[0] <= end_interval){
+                end_interval = Math.max(end_interval, interval[1])
+                start_interval = Math.min(start_interval, interval[0])
+            } else {
+                result.push([start_interval, end_interval])
+                start_interval = interval[0]
+                end_interval = interval[1]
+
+            }
+
+        }
+
+        result.push([start_interval, end_interval])
+
+
+
+
+
+        return result
+    }
+}
