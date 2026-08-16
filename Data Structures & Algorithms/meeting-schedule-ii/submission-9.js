90071d4e6a7933ca11365d793ae7e8d2ed117f37
Techin chompooborisuth <chompooborisuthtechin@gmail.com>
2026-08-16 22:36:40 +0700

    Add: meeting-schedule-ii - submission-9

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-9.js
@@ -0,0 +1,41 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        const start = intervals.map((i) => i.start).sort((a, b) => a - b)
+        const end = intervals.map((i) => i.end).sort((a, b) => a - b)
+        // [0,5,15] , [10, 20, 40]
+        let maxSchedule = 0
+        let startPointer = 0
+        let endPointer = 0 
+        let count = 0
+
+        while (startPointer < intervals.length){
+            if (start[startPointer] < end[endPointer]){
+                count += 1
+                maxSchedule = Math.max(maxSchedule, count)
+                startPointer += 1
+            } else if (start[startPointer] > end[endPointer]) {
+                count -= 1
+                endPointer += 1
+            } else {
+                count -= 1
+                endPointer += 1
+            }
+        }
+
+        return maxSchedule
+    }
+}
